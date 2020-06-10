213fb4c92a37e1c976291224ec45f96ceb609605
Netto1991 <henry17@mail.ru>
2020-06-10 23:21:12 +0400

    fix Procfile, .gitignore, wsgi.py

--- fotoksu/wsgi.py
+++ fotoksu/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fotoksu.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/static/')
+application.add_files('/static/more/', prefix='more-files/')
